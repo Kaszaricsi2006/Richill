a44e06f5f02da25e73faca606d08e8afe454c05c
Kaszaricsi2006 <90178365+Kaszaricsi2006@users.noreply.github.com>
2023-11-29 22:15:26 +0100

    Update richill.py

--- richill.py
+++ richill.py
@@ -18,12 +18,15 @@ def main():
 
     if choice == 'Create':
         st.subheader('Hozzáadás új felhasználóhoz')
-        name = st.text_input('Név', value='', max_chars=50, help='Írd be a nevet', key='name_input', encoding='utf-8')
+        name = st.text_input('Név', value='', max_chars=50, help='Írd be a nevet', key='name_input')
         age = st.number_input('Kor', 0)
         if st.button('Felhasználó hozzáadása'):
-            c.execute('INSERT INTO users (name, age) VALUES (?, ?)', (name, age))
+            # Kézi karakterkódolás az ékezetes karakterek kezelésére
+            encoded_name = name.encode('utf-8')
+            decoded_name = encoded_name.decode('utf-8')
+            c.execute('INSERT INTO users (name, age) VALUES (?, ?)', (decoded_name, age))
             conn.commit()
-            st.success('Felhasználó hozzáadva: {} - {}'.format(name, age))
+            st.success('Felhasználó hozzáadva: {} - {}'.format(decoded_name, age))
 
     elif choice == 'Read':
         st.subheader('Felhasználók listázása')
@@ -36,12 +39,15 @@ def main():
         users = c.execute('SELECT * FROM users')
         user_list = [row[1] for row in users]
         selected_user = st.selectbox('Válassz felhasználót', user_list)
-        new_name = st.text_input('Új név', value='', max_chars=50, help='Írd be az új nevet', key='new_name_input', encoding='utf-8')
+        new_name = st.text_input('Új név', value='', max_chars=50, help='Írd be az új nevet', key='new_name_input')
         new_age = st.number_input('Új kor', 0)
         if st.button('Felhasználó frissítése'):
-            c.execute('UPDATE users SET name=?, age=? WHERE name=?', (new_name, new_age, selected_user))
+            # Kézi karakterkódolás az ékezetes karakterek kezelésére
+            encoded_new_name = new_name.encode('utf-8')
+            decoded_new_name = encoded_new_name.decode('utf-8')
+            c.execute('UPDATE users SET name=?, age=? WHERE name=?', (decoded_new_name, new_age, selected_user))
             conn.commit()
-            st.success('Felhasználó frissítve: {} - {}'.format(new_name, new_age))
+            st.success('Felhasználó frissítve: {} - {}'.format(decoded_new_name, new_age))
 
     elif choice == 'Delete':
         st.subheader('Felhasználó törlése')
