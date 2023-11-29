e0603f50671c4cd40e053469f826a57906af256c
Kaszaricsi2006 <90178365+Kaszaricsi2006@users.noreply.github.com>
2023-11-29 22:01:28 +0100

    Update richill.py

--- richill.py
+++ richill.py
@@ -1,22 +1,57 @@
 import streamlit as st
-import pandas as pd
-from PIL import Image
+import sqlite3
 
+# Adatbázis kapcsolat létrehozása
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+# Tábla létrehozása, ha még nem létezik
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
+
+# Streamlit alkalmazás létrehozása
 def main():
-    st.title("Kép és Excel táblázat megjelenítése a Streamlitben")
+    st.title('SQL Adatbázis Streamlit Alkalmazás')
 
-    # Kép beolvasása
-    image = Image.open('https://th.bing.com/th/id/R.c8c6dda0a605d7dd6d0daa943ea3c691?rik=%2bg9eponkh4YkRA&riu=http%3a%2f%2fwww.pgiengineering.com%2fmedia%2fcitys%2fmexido-df.jpg&ehk=6QmdBwVK2y%2fl6x8z18lh4igOKtQ50VOc6muEUTroCI4%3d&risl=&pid=ImgRaw&r=0')  # Itt cseréld le a 'sample_image.jpg'-t a saját képed nevére
+    menu = ['Create', 'Read', 'Update', 'Delete']
+    choice = st.sidebar.selectbox('Válassz műveletet:', menu)
 
-    # Kép megjelenítése
-    st.image(image, caption='Ez egy példa kép', use_column_width=True)
+    if choice == 'Create':
+        st.subheader('Hozzáadás új felhasználóhoz')
+        name = st.text_input('Név', '')
+        age = st.number_input('Kor', 0)
+        if st.button('Felhasználó hozzáadása'):
+            c.execute('INSERT INTO users (name, age) VALUES (?, ?)', (name, age))
+            conn.commit()
+            st.success('Felhasználó hozzáadva: {} - {}'.format(name, age))
 
-    # Excel fájl beolvasása
-    excel_data = pd.read_excel('weblap.xlsx')  # Itt cseréld le a 'sample_excel.xlsx'-t a saját Excel fájlod nevére
+    elif choice == 'Read':
+        st.subheader('Felhasználók listázása')
+        result = c.execute('SELECT * FROM users')
+        for row in result:
+            st.write(f"Név: {row[1]}, Kor: {row[2]}")
 
-    # Excel táblázat megjelenítése
-    st.write("Excel táblázat:")
-    st.dataframe(excel_data)
+    elif choice == 'Update':
+        st.subheader('Felhasználó frissítése')
+        users = c.execute('SELECT * FROM users')
+        user_list = [row[1] for row in users]
+        selected_user = st.selectbox('Válassz felhasználót', user_list)
+        new_name = st.text_input('Új név', '')
+        new_age = st.number_input('Új kor', 0)
+        if st.button('Felhasználó frissítése'):
+            c.execute('UPDATE users SET name=?, age=? WHERE name=?', (new_name, new_age, selected_user))
+            conn.commit()
+            st.success('Felhasználó frissítve: {} - {}'.format(new_name, new_age))
 
-if _name_ == "_main_":
+    elif choice == 'Delete':
+        st.subheader('Felhasználó törlése')
+        users = c.execute('SELECT * FROM users')
+        user_list = [row[1] for row in users]
+        user_to_delete = st.selectbox('Válassz felhasználót törléshez', user_list)
+        if st.button('Felhasználó törlése'):
+            c.execute('DELETE FROM users WHERE name=?', (user_to_delete,))
+            conn.commit()
+            st.warning('Felhasználó törölve: {}'.format(user_to_delete))
+
+if __name__ == '__main__':
     main()
